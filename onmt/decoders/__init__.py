8c6056bdd1e63c64e05b7c24bc0f644528a49dea
Kwak Jeong Won <37138563+jeongwonkwak@users.noreply.github.com>
2020-04-15 23:49:10 +0900

    Update __init__.py

--- onmt/decoders/__init__.py
+++ onmt/decoders/__init__.py
@@ -2,11 +2,8 @@
 from onmt.decoders.decoder import DecoderBase, InputFeedRNNDecoder, \
     StdRNNDecoder
 from onmt.decoders.transformer import TransformerDecoder
-from onmt.decoders.cnn_decoder import CNNDecoder
 
 
-str2dec = {"rnn": StdRNNDecoder, "ifrnn": InputFeedRNNDecoder,
-           "cnn": CNNDecoder, "transformer": TransformerDecoder}
+str2dec = {"transformer": TransformerDecoder}
 
-__all__ = ["DecoderBase", "TransformerDecoder", "StdRNNDecoder", "CNNDecoder",
-           "InputFeedRNNDecoder", "str2dec"]
+__all__ = ["DecoderBase", "TransformerDecoder"]
